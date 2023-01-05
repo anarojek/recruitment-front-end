1b8a7c72f09b2d2160b3c8e48d0d62042482dc16
Aleksandra <aleksandra.narojek12@gmail.com>
2023-01-05 13:12:34 +0100

    Last changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,24 @@
-/** INSERT CUSTOM JS HERE **/
-export default function() {
+// /** INSERT CUSTOM JS HERE **/
+// export default function() {
 
-}
+// }
+
+function playVid() {
+    console.log("cosss");
+    let vid = document.getElementById("myFrame"); 
+    vid.style.width = "100%";
+    vid.style.height = "100%";
+
+} 
+
+const button = document.getElementById("playVideo");
+
+button.addEventListener("click", ()=>{
+    button.style.visibility= "hidden";
+ });
+
+ const watchDemo = document.getElementById("watchDemo");
+
+button.addEventListener("click", ()=>{
+    watchDemo.style.visibility= "hidden";
+ });
